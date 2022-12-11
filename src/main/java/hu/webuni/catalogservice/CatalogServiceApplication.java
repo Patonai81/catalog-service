5cf8af290b63f33ebdbee4ab891882966a1a1bed
patonaiszabolcs <patonai.szabolcs@gmail.com>
2022-12-11 14:33:06 +0100

    TaskII- remove duplicates

--- src/main/java/hu/webuni/catalogservice/CatalogServiceApplication.java
+++ src/main/java/hu/webuni/catalogservice/CatalogServiceApplication.java
@@ -1,12 +1,7 @@
 package hu.webuni.catalogservice;
 
-import hu.webuni.catalogservice.dto.CategoryDTO;
-import hu.webuni.catalogservice.dto.ProductDTO;
-import org.json.simple.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.HashSet;
 
 @SpringBootApplication
 public class CatalogServiceApplication {
@@ -15,25 +10,6 @@ public class CatalogServiceApplication {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
 
-
-    public static void main2(String[] args) {
-        JSONObject json = new JSONObject();
-        ProductDTO productDTO = new ProductDTO();
-        CategoryDTO categoryDTO = new CategoryDTO();
-        categoryDTO.setName("mycategory");
-
-       productDTO.setCategory(categoryDTO);
-        productDTO.setName("productname");
-
-        json.put("product", productDTO);
-      //  json.put("list", Arrays.asList("a", "b"));
-        String jsonString = json.toString();
-
-        System.out.println(jsonString);
-
-     //   JSONObject afterParse = new JSONObject(jsonString);
-     //   System.out.println(afterParse.toString());
-    }
 
 }
 
