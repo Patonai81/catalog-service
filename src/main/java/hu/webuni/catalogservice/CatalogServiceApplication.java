9602f9fb496a3cd78ae05f931522c9a50c230446
patonaiszabolcs <patonai.szabolcs@gmail.com>
2022-12-14 12:17:18 +0100

    TaskV- catalog service additional security

--- src/main/java/hu/webuni/catalogservice/CatalogServiceApplication.java
+++ src/main/java/hu/webuni/catalogservice/CatalogServiceApplication.java
@@ -1,9 +1,10 @@
 package hu.webuni.catalogservice;
 
+import hu.webuni.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {CatalogServiceApplication.class, SecurityConfig.class})
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
